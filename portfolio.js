4f7dc206352be822e8a9ee6774f7655ae08a461e
Kartik Hande <handekartik181@gmail.com>
2025-04-03 23:51:38 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const typed = new Typed('.typed-text', {
+        strings: ['FabLab Member', 'SSGMCE Student'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+
